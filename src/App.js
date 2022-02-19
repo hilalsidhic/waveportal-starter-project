70ab782dd5637ea096dc264f2e401035653da89b
hilalsidhic <hilalsidhic21@gmail.com>
2022-02-20 02:39:07 +0530

    ethereum request processed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import * as React from "react";
 import { ethers } from "ethers";
+import {useEffect,useState} from "react";
 import './App.css';
 
 export default function App() {
+    const [currentAccount, setcurrentAccount] = useState("") // Account address
+    const checkIfWindowIsConnected = async ()=>{
+      try {
+        const {ethereum} = window;
+      if(!ethereum){
+        console.log('make sure metamask is connected');
+      }
+      else{
+        console.log('window is connected',ethereum);
+      }
+    const getaccount = await ethereum.request({method: "eth_accounts"});
+    if (getaccount.length !== 0) {
+      console.log("Found an authorized account:", account);
+      const account = getaccount[0];
+      setcurrentAccount(account);
+    }
+    else {
+      console.log("No authorized account found");
+    }
 
-  const wave = () => {
-    
+    } catch(err){
+      console.log(err);
+    }
   }
+  useEffect(() => {
+    checkIfWindowIsConnected();
+  }, [])
+
   
   return (
     <div className="mainContainer">
@@ -20,7 +45,7 @@ export default function App() {
         I am Hilal. Connect your Ethereum wallet and wave at me!
         </div>
 
-        <button className="waveButton" onClick={wave}>
+        <button className="waveButton" >
           Wave at Me
         </button>
       </div>
